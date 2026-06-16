c6baf84c69381af6ce423d5f3242a2e2feb5c6b2
production.server <server@kompymascotas.com>
2026-06-16 13:25:04 +0200

    Desactiva el log del módulo kpygoogletags

--- modules/kpygoogletags/src/Configuration/KpyGoogleTagsConfiguration.php
+++ modules/kpygoogletags/src/Configuration/KpyGoogleTagsConfiguration.php
@@ -4,7 +4,7 @@ namespace PrestaShop\Module\KpyGoogleTags\Configuration;
 
 class KpyGoogleTagsConfiguration
 {
-    public const bool LOG_ALL = true;
+    public const bool LOG_ALL = false;
     public const bool LOG_PURCHASE = false;
     public const bool LOG_VIEW_ITEM = false;
     public const bool LOG_VIEW_CART = false;
