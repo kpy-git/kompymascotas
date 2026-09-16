5cbfac7e7a641f4b98f1dabbb5dc4e61f94a492a
Julian Bajo <desarrollo@kompymascotas.com>
2026-09-16 15:12:49 +0000

    Cambia la consulta para obtener los pedidos que pueden ser finalizados

--- modules/kpyorderdispatcher/src/Repository/OrderRepository.php
+++ modules/kpyorderdispatcher/src/Repository/OrderRepository.php
@@ -7,12 +7,11 @@ class OrderRepository
     public function findOrdersToBeFinished(): array
     {
         $results = \Db::getInstance()->executeS(
-            "select o.id_order
-                from " . _DB_PREFIX_ . "orders o
-                inner join " . _DB_PREFIX_ . "order_history oh
-                    on oh.id_order = o.id_order and oh.id_order_state = o.current_state
-                where o.current_state = 5
-                    AND DATEDIFF(CURDATE() , DATE_FORMAT(oh.date_add, '%Y-%m-%d')) >= 14"
+            "select oh.id_order
+                from " . _DB_PREFIX_ . "order_history oh
+                where oh.id_order_state = 5
+                    AND DATEDIFF(CURDATE(), DATE_FORMAT(oh.date_add, '%Y-%m-%d')) >= 14
+                    AND NOT EXISTS (SELECT 1 from " . _DB_PREFIX_ . "orders o where o.id_order = oh.id_order and o.current_state = 22)"
         );
 
         return array_map(static fn (array $row): int => $row['id_order'], $results);
